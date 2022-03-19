3f535eb02e313867633a0b502b6301732cd766ff
Tshimangadzo <mudzanani27@gmail.com>
2022-03-18 18:49:03 -0700

    added upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+
+/**
+ * main - check the code.
+ *
+ * Return:  0.
+ */
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    if (_isupper(c))
+    {
+        return (1)
+    }
+
+    return (0);
+}
