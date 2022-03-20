6d639e9189ff6e73ef8ec799664f0ec3e8d55814
Tshimangadzo <mudzanani27@gmail.com>
2022-03-20 06:46:03 -0700

    expect e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print a-z except e and q in lower case
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e'){
+			putchar(i);
+        }
+		
+	}
+	putchar('\n');
+	return (0);
+}
