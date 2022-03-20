d8afac993ca10b13692a7038dbae9c6c52823988
Tshimangadzo <mudzanani27@gmail.com>
2022-03-20 07:15:28 -0700

    added base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print numbers from 0 to 9
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
