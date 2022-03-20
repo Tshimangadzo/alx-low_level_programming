d77e813b133984c7858f6f6229f099c0c1d67b30
Tshimangadzo <mudzanani27@gmail.com>
2022-03-20 06:22:52 -0700

    added alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print a-z in lower case
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+    char i;
+
+    for (i = 'a'; i < 'z'; i++)
+    {
+        putchar(i);
+    }
+    putchar('\n');
+    return (0);
+}
