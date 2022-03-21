86a16b128ffdcd94bee823fbc00d0537b94c23d6
Tshimangadzo <mudzanani27@gmail.com>
2022-03-20 19:54:40 -0700

    added 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints to 98
+ * @n: print from this number
+ */
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			if (i == 98)
+			{
+				printf("%d\n", i);
+			}
+
+		}
+	}
+	if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+			{
+				printf("%d, ", j);
+			}
+
+			if (j == 98)
+			{
+				printf("%d\n", j);
+			}
+
+		}
+	}
+}
