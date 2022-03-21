f14a702116e5bb2dfa6304da89768c35901f6ad4
Tshimangadzo <mudzanani27@gmail.com>
2022-03-20 17:21:10 -0700

    added alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
 
 #include "main.h"
+
 /**
- * main - print a-z in lower case
- *
- * Return: 0 on success
+ * print_alphabet - prints lowercase alphabet
+ * Return: 0
  */
+
 void print_alphabet(void)
 {
 	int i;
