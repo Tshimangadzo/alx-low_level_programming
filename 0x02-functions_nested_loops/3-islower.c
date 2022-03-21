050dba7e90a7f117a35663f8c8a86343e7ea1957
Tshimangadzo <mudzanani27@gmail.com>
2022-03-20 17:44:52 -0700

    added is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,9 @@
 #include "main.h"
 
 /**
- * _islower - return 1 if c is lower otherwise return 0
- * Return: 0
+ * _islower(int c) - checks upper case
+ * @c: character to check
+ * Return :1 if c is lower otherwise return 0
  */
 
 int _islower(int c)
@@ -14,10 +15,11 @@ int _islower(int c)
 	{
 		for (i = 'a'; i <= 'z'; i++)
 		{
-			if(c == i){
+			if (c == i)
+			{
 				return (1);
 			}
 		}
 	}
-	return(0);
+	return (0);
 }
