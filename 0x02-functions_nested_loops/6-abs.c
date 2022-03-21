3f594ea6273c121bc0ba005147ccedc5d4eef222
Tshimangadzo <mudzanani27@gmail.com>
2022-03-20 18:20:57 -0700

    added abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+
+/**
+ * _abs - checks for negative
+ * @n: character to check
+ * Return: positive numbers
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return ((-1) * n);
+	}
+
+	return (n);
+}
