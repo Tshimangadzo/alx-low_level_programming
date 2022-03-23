81fb796692cf40494f4d30ad1321013e6a9ff9c6
Tshimangadzo <mudzanani27@gmail.com>
2022-03-22 20:06:42 -0700

    added puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints some numbers
+ * @str: string
+ * 
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0';)
+	{
+		_putchar(str[i]);
+		 i = i + 2;
+	}
+	_putchar('\n');
+}
+
+
