6962598b06bf3ac180168fc8c41cb3339365dac0
Tshimangadzo <mudzanani27@gmail.com>
2022-03-22 18:23:12 -0700

    added string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _strlen - return length
+ *@s: char argument
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i, count = 0;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return count;
+}
