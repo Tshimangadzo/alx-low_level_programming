7386bd3aed89ce32ccc0d5a7e119f8e800dfe3ba
Tshimangadzo <mudzanani27@gmail.com>
2022-03-22 19:00:15 -0700

    added print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints
+ *@s: char argument
+ *
+ *
+ */
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (i = count; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
+
+
