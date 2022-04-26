b4b9fd6c1489710336b2ac50faae254eb9c7de4f
Tshimangadzo <mudzanani27@gmail.com>
2022-04-26 04:19:18 -0700

    added list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,22 +1,23 @@
 #include "lists.h"
 
+
 /**
  * listint_len - return number of nodes.
  * @h: pointer to a structure typedef list_t
  * Return: the number of nodes
  **/
 
+
 size_t listint_len(const listint_t *h)
 {
-	/*temp is a listint_t type. It is a structure*/
+	size_t count = 0;
 	const listint_t *cont = h;
-	size_t numNodes = 0;
 
 	while (cont != NULL)
 	{
 		cont = cont->next;
-		numNodes++;
+		count = count + 1;
 	}
 
-	return (numNodes);
+	return (count);
 }
